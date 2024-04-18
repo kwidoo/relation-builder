b0042b25d4ee772dc9cb5ce030f8281a35443759
Oleg Pashkovsky <oleg@pashkovsky.me>
2024-04-18 10:54:01 +0300

    Add Morph  To not implemented

--- src/components/relations/MorphTo.vue
+++ src/components/relations/MorphTo.vue
@@ -1,25 +1,82 @@
 <template>
   <v-container>
-    <!-- Empty template for morphTo relationship -->
-    <h1>morphTo Page</h1>
+    <v-form v-if="false">
+      <v-container fluid class="px-0">
+        <v-row justify="center">
+          <morph-id-input v-model="morphId" label="Morph ID" md="4"/>
+          <morph-type-input v-model="morphType" label="Morph Type" md="4"/>
+        </v-row>
+        <options-bar v-model:asTrait="asTrait" v-model:withScopes="withScopes" md="4"/>
+      </v-container>
+    </v-form>
+    <code-component v-if="false"
+    :codeText="output" :filename="filename" />
+    <h1>Not Implemented</h1>
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { debounce } from 'lodash';
+import { defineComponent, ref, onMounted, watch, computed } from 'vue';
+import { CodeComponent, OptionsBar } from '@components';
+import { useAppStore } from '@/store/app';
+import { useAsTrait, useConstants } from '@composables';
 
 export default defineComponent({
-  name: 'morphToPage',
+  name: 'MorphToRelationBuilder',
+  components: {
+    CodeComponent,
+    OptionsBar,
+
+  },
   setup() {
-    // Setup code for morphTo
+    const appStore = useAppStore();
+    const { MORPH_TO, morphToStub, morphToScopes } = useConstants();
+    let output = ref('');
+
+    const morphId = ref(''); // Identifier for the polymorphic relation
+    const morphType = ref(''); // Type for the polymorphic relation
+
+    const asTrait = ref(true);
+    const withScopes = ref(false);
+    const namespace = computed(() => appStore.getNamespace);
+
+    const generateRelation = async () => {
+      output.value = '';
+
+      const initialStub = await morphToStub();
+      let finalOutput = initialStub.replace(/{{\s*scopes\s*}}/g, withScopes.value ? await morphToScopes() : '');
+
+      finalOutput = finalOutput
+        .replace(/{{\s*morphId\s*}}/g, morphId.value)
+        .replace(/{{\s*morphType\s*}}/g, morphType.value);
+      
+      if (asTrait.value) {
+        const { trait } = await useAsTrait();
+         finalOutput = trait(namespace.value, 'tags', finalOutput);
+      }
+
+      output.value = finalOutput;
+    };
+
+    watch(() => [
+      morphId.value,
+      morphType.value,
+      namespace.value,
+      asTrait.value,
+      withScopes.value,
+    ], debounce(generateRelation, 300));
+
+    onMounted(generateRelation);
 
     return {
-      // Data and methods for morphTo
+      morphId,
+      morphType,
+      output,
+      asTrait,
+      withScopes,
+      filename: 'MorphToRelation' // Generic filename as the specific model isn't known
     };
-  }
+  },
 });
 </script>
-
-<style scoped>
-/* Styles for morphTo page */
-</style>
